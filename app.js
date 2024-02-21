ca0f0027bb2eddfbded09b55fe638ea0f4f07082
AndyWilliam96 <153957322+andybrocklesby@users.noreply.github.com>
2024-02-20 19:37:40 -0600

    Finished base content

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js" 
+import { getDatabase, ref, push, onValue, remove  } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-bf40c-default-rtdb.firebaseio.com/"
@@ -7,63 +7,86 @@ const appSettings = {
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-const postListInDB = ref(database, "postList");
+const postListInDB = ref(database, 'postList');
 
-const fromEl = document.getElementById("from-input-el");
-const toEl = document.getElementById("to-input-el");
-const endorsementEl = document.getElementById("endorsement-input-el");
-const publishBtn = document.getElementById("submit-btn-el");
+const endorsementInputElement = document.getElementById("endorsement-input-el");
+const fromInputElement = document.getElementById("from-input-el");
+const toInputElement = document.getElementById("to-input-el");
 
-const locationToAppend = document.getElementById("endorsement-display");
+const publishButton = document.getElementById("publish-btn-el");
 
-publishBtn.addEventListener("click", function(){
-    clearList();
-    let post = {
-        sender: fromEl.value,
-        recipient: toEl.value,
-        endorsement: endorsementEl.value
-    }
+//-----------------------PUSHING DATA---------------------------
+
+publishButton.addEventListener("click", function(){
     
-    push(postListInDB, post);
+    let newPost = {
+        endorsement: endorsementInputElement.value,
+        from: fromInputElement.value,
+        to: toInputElement.value
+    }
+
+    push(postListInDB, newPost);
+
+    appendNewEndorsement();
+
+
 })
+
+//-----------------------ONVALUE-------------------------------
 
 onValue(postListInDB, function(snapshot){
-    clearList();
-    let postArray = Object.entries(snapshot.val());
+
+    resetElements();
+
+    if(snapshot.exists()){
+        let postArrayInDB = Object.entries(snapshot.val());
+
+        for(let i = 0; i < postArrayInDB.length; i++){
     
-    for(let i = 0; i < postArray.length; i++){
-        let postID = postArray[i][0];
-        let postObject = postArray[i][1];
-        console.log(postID);
-        console.log(postObject);
-        
-        let newEndorsement = document.createElement("div");
-        newEndorsement.className = "endorsement";
-        newEndorsement.innerHTML = `
-            <div class="endorsement-name">From: ${postObject.sender}</div>    
-            <div class="endorsement-content">${postObject.endorsement}</div>    
-            <div class="endorsement-name">To: ${postObject.recipient}</div>    
-        `
-        let locationToAppend = document.getElementById("endorsement-display");
-        locationToAppend.append(newEndorsement);
-        
-        newEndorsement.addEventListener("dblclick", function(){
-            let exactLocationInDB = ref(database, `postList/${postID}`);
-            remove(exactLocationInDB);
-        })
+            let postID = postArrayInDB[i][0];
+            let postValue = postArrayInDB[i][1];
+    
+            appendNewEndorsement(postValue, postID);
+        }
     }
+    
 })
 
-function clearList(){
-    locationToAppend.innerHTML = "";
+//------------------APPEND NEW ENDORSEMENT--------------------
+
+function appendNewEndorsement(post, postID){
+
+    let newEndorsement = document.createElement('div');
+        newEndorsement.className = 'endorsement';
+        newEndorsement.innerHTML = `
+            <div class="endorsement-name">To: ${post.from}</div>
+            <div>${post.endorsement}</div>
+            <div class="endorsement-name">From: ${post.to}</div>
+        `
+
+        let parentToAppendTo = document.getElementById("endorsement-display");
+        parentToAppendTo.append(newEndorsement);
+
+        newEndorsement.addEventListener("dblclick", function(){
+            removeElement(postID);
+        })
 }
 
-// I have tried adding the like button feature for over 12 hours to the point of getting frustrated with myself. 
-//I have tried looking online but it's telling me to use real world features such as transactions and what not. 
-//I wanted to apply what we have already learned. I could have used localStorage, however I wanted to use the
-// firebase database to save and update. I just could not figure it out. This scrim review I don't really want
-// it to focus on my overall layout. I frankly just need help with the like button feature and how I should have implemented it. 
-//Once I figure that out, I'll refactor everything else and deploy it to Netlify. :)
+//-----------------------REMOVE ENDORSEMENT-------------------
+
+function removeElement(postID){
+    let exactLocationInDB = ref(database, `postList/${postID}`);
+    remove(exactLocationInDB);
+}
+
+//---------------------------RESET ELEMENTS--------------------
+
+function resetElements(endorsementElement){
+    let parentToAppendTo = document.getElementById("endorsement-display");
+    parentToAppendTo.innerHTML = "";
+}
+
+
 
 
 
